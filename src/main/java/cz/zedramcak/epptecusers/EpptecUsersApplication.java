e5308a9034e21b9a4637e7a3296c367439faa05d
Zedramcak <adamzrc@gmail.com>
2024-10-28 08:19:11 +0100

    Add initial Spring Boot project setup
    Introduced a new Spring Boot project with essential configurations and dependencies. Added `.gitignore` and `.gitattributes` files for IDE and OS-specific configurations, as well as Maven Wrapper scripts for consistent builds.

---
+++ src/main/java/cz/zedramcak/epptecusers/EpptecUsersApplication.java
@@ -0,0 +1,13 @@
+package cz.zedramcak.epptecusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpptecUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpptecUsersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/zedramcak/epptecusers/EpptecUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.zedramcak.epptecusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpptecUsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
